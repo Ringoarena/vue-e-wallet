5f6badf27204f6161b2f50071b161d59dcbcb0cd
Rikard Virta <rikard.virta90@gmail.com>
2020-05-15 10:32:20 +0200

    styled credit card

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,6 +1,22 @@
 <template>
   <article>
-      <p>The actual card</p>
+    <div class="chip">
+      <img src="../assets/chip-dark.svg" alt="">
+    </div>
+    <div class="vendor" >
+      <img src="../assets/vendor-bitcoin.svg" alt="">
+    </div>
+    <div class="num">
+      <p>1234 5678 9101 1213</p>
+    </div>
+    <div class="name-section">
+      <p class="label">CARDHOLDER NAME</p>
+      <p class="name">MASTEN PERSSON</p>
+    </div>
+    <div class="expiry">
+      <p class="label">VALID THRU</p>
+      <p class="date">13/37</p>
+    </div>
   </article>
 </template>
 
@@ -13,13 +29,67 @@ export default {
 <style lang="scss" scoped>
 article {
     background: dodgerblue;
-    width: 90vw;
-    max-width: 32rem;
-    height: 56.75vw;
-    max-height: 18.915rem;
-    max-height: 20rem;
+    width: 80vw;
+    max-width: 37rem;
+    min-height: 23rem;
     border-radius: 10px;
     padding: 1rem;
-    // display: ;
+
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
+    grid-template-rows: repeat(3, auto);
+    grid-template-areas: 
+    "chip . . vendor"
+    "num num num num"
+    "name name name expiry";
+    .chip {
+      grid-area: chip;
+      display: flex;
+      img {
+        width: 5rem;
+      }
+    }
+    .vendor {
+      grid-area: vendor;
+      display: flex;
+      justify-content: center;
+      img {
+        width: 3rem;
+        margin-top: 0;
+      }
+    }
+    .num {
+      grid-area: num;
+      p {
+        margin: 1rem 0 3rem 0;
+        font-size: 3rem;
+        word-spacing: 1rem;
+        letter-spacing: 0.1em;
+      }
+    }
+    .name-section {
+      grid-area: name;
+      .label {
+        margin: 0;
+      }
+      .name {
+        margin: 0;
+        font-size: 2rem;
+        word-spacing: 1rem;
+        letter-spacing: 0.1em;
+      }
+    }
+    .expiry {
+      grid-area: expiry;
+      text-align: right;
+      .label {
+        margin: 0;
+      }
+      .date {
+        margin: 0;
+        font-size: 2rem;
+      }
+    }
+
 }
 </style>
