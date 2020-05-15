5c50820af9d7e49f00a8871461b67fd9b0f6d053
Rikard Virta <rikard.virta90@gmail.com>
2020-05-15 13:09:34 +0200

    styled form, width-settings not complete

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,10 +1,10 @@
 <template>
   <article>
     <div class="chip">
-      <img src="../assets/chip-dark.svg" alt="">
+      <img src="../assets/chip-dark.svg" alt />
     </div>
-    <div class="vendor" >
-      <img src="../assets/vendor-bitcoin.svg" alt="">
+    <div class="vendor">
+      <img src="../assets/vendor-bitcoin.svg" alt />
     </div>
     <div class="num">
       <p>1234 5678 9101 1213</p>
@@ -21,71 +21,68 @@
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
 <style lang="scss" scoped>
 article {
-    background: dodgerblue;
-    border-radius: 10px;
-    padding: 1rem;
+  background: dodgerblue;
+  border-radius: 10px;
+  padding: 1rem;
 
-    display: grid;
-    grid-template-columns: repeat(4, 1fr);
-    grid-template-rows: repeat(3, auto);
-    grid-template-areas: 
+  display: grid;
+  // grid-template-columns: repeat(4, 1fr);
+  // grid-template-rows: repeat(3, auto);
+  grid-template-areas:
     "chip . . vendor"
     "num num num num"
     "name name name expiry";
-    .chip {
-      grid-area: chip;
-      display: flex;
-      img {
-        width: 5rem;
-      }
+  .chip {
+    grid-area: chip;
+    display: flex;
+    img {
+      width: 5rem;
     }
-    .vendor {
-      grid-area: vendor;
-      display: flex;
-      justify-content: center;
-      img {
-        width: 3rem;
-        margin-top: 0;
-      }
+  }
+  .vendor {
+    grid-area: vendor;
+    display: flex;
+    justify-content: center;
+    img {
+      width: 3rem;
+      margin-top: 0;
     }
-    .num {
-      grid-area: num;
-      p {
-        margin: 1rem 0 3rem 0;
-        font-size: 3rem;
-        word-spacing: 1rem;
-        letter-spacing: 0.05rem;
-      }
+  }
+  .num {
+    grid-area: num;
+    p {
+      margin: 1rem 0 3rem 0;
+      font-size: 3rem;
+      word-spacing: 1rem;
+      letter-spacing: 0.05rem;
     }
-    .name-section {
-      grid-area: name;
-      .label {
-        margin: 0;
-      }
-      .name {
-        margin: 0;
-        font-size: 2rem;
-        word-spacing: 1rem;
-      }
+  }
+  .name-section {
+    grid-area: name;
+    .label {
+      margin: 0;
     }
-    .expiry {
-      grid-area: expiry;
-      text-align: right;
-      .label {
-        margin: 0;
-      }
-      .date {
-        margin: 0;
-        font-size: 2rem;
-      }
+    .name {
+      margin: 0;
+      font-size: 2rem;
+      word-spacing: 1rem;
     }
-
+  }
+  .expiry {
+    grid-area: expiry;
+    text-align: right;
+    .label {
+      margin: 0;
+    }
+    .date {
+      margin: 0;
+      font-size: 2rem;
+    }
+  }
 }
 </style>
--- src/components/CardForm.vue
+++ src/components/CardForm.vue
@@ -1,7 +1,30 @@
 <template>
-  <section>
-      
-  </section>
+  <article>
+    <div class="num">
+      <p>CARD NUMBER</p>
+      <input type="text">
+    </div>
+    <div class="name-section">
+      <p>CARDHOLDER NAME</p>
+      <input type="text">
+    </div>
+    <div class="expiry">
+      <p>VALID THRU</p>
+      <input type="text">
+    </div>
+    <div class="ccv">
+      <p>CCV</p>
+      <input type="text">
+    </div>
+    <div class="vendor">
+      <p>VENDOR</p>
+      <input type="text">
+    </div>
+    <div class="submit">
+      <button>ADD CARD</button>
+    </div>
+
+  </article>
 </template>
 
 <script>
@@ -11,5 +34,60 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+article {
+  // border: 1px solid red;
+  width: 90%;
+
+  margin-top: 5rem;
+  display: grid;
+  column-gap: 2rem;
+  row-gap: 2rem;
+  grid-template-areas: 
+  "num num"
+  "name name"
+  "expiry ccv"
+  "vendor vendor"
+  "submit submit";
+  p {
+    margin: 0 0 0.3rem 0;
+  }
+  input {
+    padding: 1rem;
+    border-radius: 10px;
+    border: 2px solid black;
+    box-sizing: border-box;
+    width: 100%;
+  }
+  .num {
+    grid-area: num;
+    input {
+      
+    }
+  }
+  .name-section {
+    grid-area: name;
+  }
+  .expiry {
+    grid-area: expiry;
+  }
+  .ccv {
+    grid-area: ccv;
+  }
+  .vendor {
+    grid-area: vendor;
+  }
+  .submit {
+    grid-area: submit;
+    button {
+      margin: 1rem 0;
+      width: 100%;
+      border: none;
+      border-radius: 10px;
+      padding: 1rem;
+      background-color: rgb(70, 70, 70);
+      color: white;
+    }
+  }
+}
 
 </style>
